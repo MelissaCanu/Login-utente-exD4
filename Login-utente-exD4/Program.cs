e47ce0230757caeb0c81a8d266049739f916f553
MelissaCanu <melissamcanu@gmail.com>
2024-02-01 16:14:11 +0100

    program.cs complete, menu, do while e switch

--- Login-utente-exD4/Program.cs
+++ Login-utente-exD4/Program.cs
@@ -10,6 +10,73 @@ namespace Login_utente_exD4
     {
         static void Main(string[] args)
         {
+        // dichiarazione variabili
+            string username;
+            string password; 
+            string confermaPassword; 
+            string scelta; 
+            bool login = false; 
+            bool logout = false; 
+            DateTime lastLogin = DateTime.MinValue; // valore di default di un DateTime 
+            bool exit = false; // variabile booleana per uscire dal ciclo do-while
+            do // ciclo do-while per il menu
+            {
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Logout");
+                Console.WriteLine("3. Last Login");
+                Console.WriteLine("4. Exit");
+                scelta = Console.ReadLine(); 
+                switch (scelta) // switch per le scelte del menu
+                {
+                    case "1": 
+                        Console.WriteLine("Inserisci username");
+                        username = Console.ReadLine();
+                        Console.WriteLine("Inserisci password");
+                        password = Console.ReadLine();
+                        Console.WriteLine("Conferma password");
+                        confermaPassword = Console.ReadLine();
+                        login = Utente.Login(username, password, confermaPassword); // chiamata al metodo statico Login
+                        if (login == true) 
+                        {
+                            Console.WriteLine("Login effettuato");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Errore, login non effettuato");
+                        }
+                        break;
+                    case "2":
+                        logout = Utente.Logout(); // chiamata al metodo statico Logout
+                        if (logout == true)
+                        {
+                            Console.WriteLine("Logout effettuato");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Errore, logout non effettuato");
+                        }
+                        break;
+                        case "3":
+                             Console.WriteLine("Ultimo login effettuato: " + Utente.LastLogin()); // chiamata al metodo statico LastLogin
+                   
+                        break;
+                    case "4": // uscita dal ciclo do-while
+                        exit = true;
+                        break;
+                    default: 
+                        Console.WriteLine("Scelta non valida");
+                        break;
+                }
+            } while (exit == false);
+           
+
+
+
+
+
+
+
+
         }
     }
 }
---
+++ Login-utente-exD4/Utente.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_utente_exD4
+{
+    public static class Utente
+    {   // dichiarazione variabili
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+
+        // metodo statico con tre parametri di tipo stringa e valore di ritorno booleano
+        public static bool Login(string username, string password, string ConfermaPassword)
+        { 
+        if (username == "" || password == "" || ConfermaPassword == "")
+            {
+                return false;
+            }
+            else if (password != ConfermaPassword)
+            {
+                return false;
+            }
+            else
+            {
+                Username = username;
+                Password = password;
+                return true;
+            }
+        }
+        // metodo statico logout con nessun parametro e valore di ritorno booleano, ritorna messaggio di errore se utente non è loggato
+        public static bool Logout()
+        {
+            if (Username == null || Password == null)
+            {
+                Console.WriteLine("Errore, utente non loggato");
+                return false;
+            }
+            else
+            {
+                Username = null;
+                Password = null;
+                return true;
+            }
+        }
+
+        // metodo statico datetime con nessun parametro e valore di ritorno stringa, ritorna data e ora ultimo login
+        public static string LastLogin()
+        {
+            if (Username == null || Password == null)
+            {
+                Console.WriteLine("Errore, utente non loggato");
+                return null;
+            }
+            else
+            {
+                DateTime now = DateTime.Now;
+                string data = now.ToString("dd/MM/yyyy HH:mm:ss");
+                return data;
+            }
+        }
+
+        // metodo statico  con nessun parametro e valore di ritorno stringa, lista dei login dell'utente
+        public static List<string> LoginHistory() 
+        {
+            List<string> lista = new List<string>(); // dichiarazione lista di stringhe
+            if (Username == null || Password == null)
+            {
+                Console.WriteLine("Errore, utente non loggato");
+                return null;
+            }
+            else
+            {
+                lista.Add(LastLogin());
+                return lista;
+            }
+        }
+
+        
+    }
+}
